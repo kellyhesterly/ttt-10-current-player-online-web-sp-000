da9ab108fe077ef8a5db3c44882c2bf45bc60cee
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-26 21:24:21 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,17 @@
 def turn_count(board)
-  count = 0
-board.each {|turn|
-  if turn == "X" || turn == "O"
-  count += 1
-end
-}
-count
+  counter = 0
+
+board.count do |element|
+  if element == "X" || element == "O"
+  counter += 1
+    end
+  end
 end
 
 def current_player(board)
-if turn_count(board) % 2 == 0
-"X"
-else
-"O"
-end
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
 end
